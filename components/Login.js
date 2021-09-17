2a81108910a93ef0f81574add597023d949f5ea1
abdul riyaz <45689131+abdulriiyaz@users.noreply.github.com>
2021-09-18 02:10:21 +0530

    header ui and facebook auth

---
+++ components/Login.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import { signIn } from 'next-auth/client'
+
+function Login() {
+    return (
+        <div className="grid place-items-center">
+            <Image
+            src="https://links.papareact.com/t4i"
+            objectFit="contain"
+            width={400}height={400} alt='Login'/>
+            <button 
+            onClick={signIn}
+            className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</button>
+        </div>
+    )
+}
+
+export default Login
